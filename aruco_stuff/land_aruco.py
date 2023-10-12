80dc1ed0c30814f58dae0ac89fd6ae3b04796744
camchaney <cameron.chaney.vssa@gmail.com>
2023-10-11 17:41:11 -0700

    lat lon case fix

--- aruco_stuff/land_aruco.py
+++ aruco_stuff/land_aruco.py
@@ -55,7 +55,7 @@ def get_location_metres(original_location, dNorth, dEast):
     dLat = dNorth/earth_radius
     dLon = dEast/(earth_radius*math.cos(math.pi*original_location.lat/180))
     
-    print(f"{dlat}, {dlon}", dLat, dLon)
+    print(f"{dLat}, {dLon}", dLat, dLon)
 
     #New position in decimal degrees
     newlat = original_location.lat + (dLat * 180/math.pi)
